1938bb37264fa28e60e0ea772041ae34ff3df7e3
Renan Garcia <martins.renan@hotmail.com>
2021-01-13 12:20:34 -0300

    Migrations

--- Data/StoreDataContext.cs
+++ Data/StoreDataContext.cs
@@ -11,7 +11,7 @@ namespace ProductCatalog.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=prodcat;User ID=SA;Password=KD=vj7!2'C$a>C;>");
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=prodcat;User ID=SA;Password=eUR3yMdZ");
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
